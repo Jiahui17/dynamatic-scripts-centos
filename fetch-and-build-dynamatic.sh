63e21568cf1b2cb684f38c631df96ac931e82be4
Jiahui Xu <jxu@ethz.ch>
2023-11-09 12:36:28 +0100

    update build script and rebuild script

--- fetch-and-build-dynamatic.sh
+++ fetch-and-build-dynamatic.sh
@@ -15,4 +15,7 @@ mkdir -p $DYNAMATIC_ROOT
 
 git clone git@github.com:EPFL-LAP/dynamatic.git $DYNAMATIC_ROOT
 
-cd $DYNAMATIC_ROOT && bash $SCRIPT_CWD/mybuild.sh && echo "successfully built dynamatic"
+
+cd $DYNAMATIC_ROOT
+git checkout origin/iterative-buffers
+bash $SCRIPT_CWD/mybuild.sh && echo "successfully built dynamatic"
